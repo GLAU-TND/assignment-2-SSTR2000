be46829c2765eb8d3193361b3daf66d5c558d579
SSTR2000 <sahil.tomar_cs18@gla.ac.in>
2020-03-28 19:03:25 +0530

    addition main class is involved for separate purpose

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,22 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree1 = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        // taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+        int a = 7;
+        while (a > 0) {
+            tree1.add(scanner.nextInt());
+            a--;
+        }
+        tree1.preOrder(tree1.root);
+        System.out.println();
+        tree1.postOrder(tree1.root);
+    }
 }
